a091805d29671cea8890667e2661723d62a82360
Elliot He <elliot.he1600@gmail.com>
2024-04-07 16:22:24 +1000

    Replace logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,4 @@
 import logo from "./logo.svg";
-import eLogo from "./eh-favicon-color.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -32,7 +31,6 @@ import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
-  eLogo,
   logo,
   backend,
   creator,
