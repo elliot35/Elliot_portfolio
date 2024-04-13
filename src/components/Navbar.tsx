7b4a667f224ffdf5201ed410eabc1b8742ad81d0
Elliot He <elliot.he1600@gmail.com>
2024-04-13 19:48:36 +1000

    Fix Tailwind not applied bug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,19 +6,35 @@ import { navLinks } from '../constants/index';
 import { logo, menu, close } from '../assets/index';
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        {/* Logo */}
         <Link
-          to='/'
-          className='flex items-center gap-2'
+          to="/"
+          className="flex items-center gap-2"
           onClick={() => {
+            setActive('');
             window.scrollTo(0, 0);
-          }}>
-            <img src={logo} alt='logo' className='w-9 h-9 object-contain' style={{ height: '5%', width: '5%' }} />
+          }}
+        >
+          <img src={logo} alt="Logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Elliot&nbsp;<span className="sm:block hidden">| Software Engineer</span>
+          </p>
         </Link>
+
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
 
     </nav>
